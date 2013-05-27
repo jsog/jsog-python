cc64a5e3494a29cb8742c801e649a0608cf4789f
Jeff Schnitzer <jeff@infohazard.org>
2013-05-27 12:33:24 -0700

    Added tests which cover circular references and nulls

--- test_jsog.py
+++ test_jsog.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestJSOG(unittest.TestCase):
 
-	def test_serialize_reference(self):
+	def test_encode_reference(self):
 		inner = { "foo": "bar" }
 		outer = { "inner1": inner, "inner2": inner }
 		encoded = jsog.encode(outer)
@@ -20,13 +20,45 @@ class TestJSOG(unittest.TestCase):
 		else:
 			self.assertEqual(inner1['@ref'], inner2['@id'])
 
-	def test_deserialize_reference(self):
+	def test_decode_reference(self):
 		JSOGIFIED = '{"@id":"1","foo":"foo","inner1":{"@id":"2","bar":"bar"},"inner2":{"@ref":"2"}}'
 		parsed = jsog.loads(JSOGIFIED)
 
 		inner1 = parsed['inner1']
 		inner2 = parsed['inner2']
 		self.assertTrue(inner1 is inner2)
+
+	def test_encode_circular(self):
+		thing = {}
+		thing['me'] = thing
+
+		encoded = jsog.encode(thing)
+
+		self.assertTrue(encoded['@id'])
+		self.assertTrue(encoded['me']['@ref'] == encoded['@id'])
+
+	def test_decode_circular(self):
+		thing = {}
+		thing['me'] = thing
+
+		encoded = jsog.encode(thing)
+		back = jsog.decode(encoded)
+
+		self.assertFalse('@id' in back)
+		self.assertTrue(back['me'] is back)
+
+	def test_encode_null(self):
+		encoded = jsog.encode(None)
+		self.assertEqual(encoded, None)
+
+	def test_decode_null(self):
+		decoded = jsog.decode(None)
+		self.assertEqual(decoded, None)
+
+	def test_decode_plain_json(self):
+		json = { "foo": "bar" }
+		decoded = jsog.decode(json)
+		self.assertEqual(json, decoded)
 
 if __name__ == '__main__':
 	unittest.main()
